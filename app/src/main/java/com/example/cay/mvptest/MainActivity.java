edb6a4c2c2394ab83fcabc2f17758d1fa78f6874
Cay <276495166@qq.com>
2017-03-25 20:24:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cay/mvptest/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.cay.mvptest;
+
+import android.os.Bundle;
+
+import com.example.cay.mvptest.bese.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/cay/mvptest/bese/BaseActivity.java
@@ -0,0 +1,49 @@
+package com.example.cay.mvptest.bese;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+
+import com.example.cay.mvptest.R;
+
+public class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "Cay";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+        initToolbar();
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.icon_back);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void setToolbarTitle(String title) {
+
+    }
+}
